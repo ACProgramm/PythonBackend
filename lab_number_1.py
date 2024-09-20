54007b77eb0f48e0c30464c76ffee65db0b7da70
Andrey <andrei-cheporov@yandex.ru>
2024-09-20 13:58:27 +0300

    Lab1 is added

---
+++ lab_number_1.py
@@ -0,0 +1,11 @@
+"""
+This module contains a function that prints 'Hello, World!' to the console.
+"""
+
+def print_hello_world():
+    """Prints Hello, World! to the console."""
+    print("Hello, World!")
+
+# Вызов функции
+if __name__ == "__main__":
+    print_hello_world()
